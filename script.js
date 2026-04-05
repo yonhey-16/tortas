2618806371ee6848ce56114dfdaa521b866f868a
yonhey-16 <yonheybonilla@gmail.com>
2026-04-05 22:59:07 +0000

    0

---
+++ script.js
@@ -0,0 +1,6 @@
+// Ejemplo simple de interacción
+document.querySelectorAll("button").forEach(btn => {
+    btn.addEventListener("click", () => {
+        alert("¡Gracias por tu interés! 🧁");
+    });
+});
